4fa516cce379a59eecdec397cc374bb388f423aa
nagarwal52 <41553748+nagarwal52@users.noreply.github.com>
2022-01-18 19:35:42 +0530

    Update CausalGraph.py

--- CausalGraph.py
+++ CausalGraph.py
@@ -11,22 +11,30 @@ from generate_synthetic_data import *
 
 class CausalGraph:
     def __init__(self, data, edges, latent_edges):
+        """
+        data : {Node : array of length N}
+        edges : List of pair of tuples, i.e., [(edge from Node A, edge to Node B)]
+        latent_edges : List of pair of tuples, i.e., [(edge from Node A, edge to Node B)]; information about the confounding structure. If not given, 
+        we asuume full confounding 
+        """
         self.data = data
         self.edges = edges
         self.latent_edges = latent_edges
         self.dag = nx.DiGraph()
-        self.nodes = list(self.data.keys())
-        type_of_actions = ['non-actionable', 'actionable', 'actionable']
-        A = dict(zip(self.nodes, type_of_actions))
+        self.nodes = list(self.data.keys()) # List of nodes in the graph; endogeneous variables 
+                
+        #type_of_actions = ['non-actionable', 'actionable', 'actionable']
+        #A = dict(zip(self.nodes, type_of_actions))
         #attrs = {'X_1': {"attr1": 20}, 'X_2': {"attr2": 3}, 'X_3': {'attr3':7}}
         nx.set_node_attributes(self.dag, A , name='types')
         #groups = set(nx.get_node_attributes(self.dag,'types').values())
 
         self.observed_index = [self.nodes.index(node)+1 for node in self.nodes]
         self.observed_dict = dict(zip(self.nodes,self.observed_index))
-        self.latent_nodes = ['U_{}'.format(i) for i in self.observed_index] 
+        self.latent_nodes = ['U_{}'.format(i) for i in self.observed_index]  # exogeneous variables 
         #self.rf_nodes = ['R_{}'.format(i) for i in self.observed_index] 
-        self.latent_index = [self.latent_nodes.index(lat_node)+1 for lat_node in self.latent_nodes]
+        self.latent_index = [self.latent_nodes.index(lat_node)+1 for 
+                             lat_node in self.latent_nodes]
         #self.nodes_names_dict = dict(zip(self.rf_nodes,self.nodes))
         self.latent_dict = dict(zip(self.latent_nodes,self.latent_index))
         #self.rf_nodes_dict = dict(zip(self.rf_nodes,self.latent_index))
@@ -38,9 +46,12 @@ class CausalGraph:
         self.edges = edges
         self.dimension = len(list(self.data.values())[0])
         #self.states = len(np.unique(list(self.data.values())[0]))
-        self.dag.add_nodes_from(self.nodes)
+        
+        # Creating graph 
+        self.dag.add_nodes_from(self.nodes) 
         self.dag.add_edges_from(self.edges)
-        self.data_nd = np.array([list(self.data.values())[i] for i in range(len(self.nodes))])
+        
+        self.data_nd = np.array([list(self.data.values())[i] for i in range(len(self.nodes))]) # Converting dictionary to arrays
         self.domain = [list(np.unique(data.get(node))) for node in self.nodes]
         self.domain_dict = dict(zip(self.nodes,self.domain))
         self.dimension = len(list(data.values())[0])
@@ -50,6 +61,8 @@ class CausalGraph:
         self.boundary_edges = [(self.nodes_with_latent.get(node),node) for node in self.nodes] 
         #self.dag.add_nodes_from(self.rf_nodes)
         self.model = gp.Model('bilinear')
+        
+        # Adding confounding structure to the graph
         self.conf_edges = []
         for (a,b) in self.latent_edges:
             (x,y) = (self.observed_dict[a],self.observed_dict[b])
@@ -65,6 +78,9 @@ class CausalGraph:
         return np.random.uniform(0,1, tuple(self.states))
     
     def draw(self):
+        """
+        Draws a causal graph with given confounding structure
+        """
         dot = graphviz.Digraph()
         for i, node in enumerate(self.nodes):
             #dot.node(node, node, {"shape": "ellipse", "color": colors[i]}, texmode="math")
@@ -85,6 +101,7 @@ class CausalGraph:
 
         return dot
     def distribution(self):
+
         jointProbs, _ = np.histogramdd(self.data_nd.T, bins=tuple(self.states))
         jointProbs /= jointProbs.sum()
         p_vec = jointProbs.flatten()
